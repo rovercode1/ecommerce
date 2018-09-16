9159085819c66d5fd2b75ca5eda5eb0f8f4d3a50
Hayden Worden <haydenworden@outlook.com>
2018-09-16 15:03:56 +0100

    Deleted theme.

---
+++ wp-content/themes/storefront-child/footer.php
@@ -0,0 +1,22 @@
+<?php do_action( 'storefront_before_footer' ); ?>
+
+<footer id="colophon" class="site-footer" role="contentinfo">
+  <div class="col-full">
+
+    <?php
+    /**
+     * Functions hooked in to storefront_footer action
+     *
+     * @hooked storefront_footer_widgets - 10
+     * @hooked storefront_credit         - 20
+     */
+    do_action( 'storefront_footer' ); ?>
+
+  </div><!-- .col-full -->
+</footer><!-- #colophon -->
+</div> <!--Loading page icon  -->
+<?php do_action( 'storefront_after_footer' ); ?>
+<!-- <script type="text/javascript" src=""></script> -->
+<?php wp_footer() ?>
+  </body>
+</html>
