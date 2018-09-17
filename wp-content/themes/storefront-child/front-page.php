66917a4aa7f8d92472245f173d6d6c512e266c08
Hayden Worden <haydenworden@outlook.com>
2018-09-17 09:52:20 +0100

    Front page header created.

--- wp-content/themes/storefront-child/front-page.php
+++ wp-content/themes/storefront-child/front-page.php
@@ -1,5 +1,9 @@
 <?php include 'header.php' ?>
-<section>
-  
+<section id='header'style= 'background-image:url( <?php echo get_theme_file_uri("assets/img/beach.png")?> )'>
+  <div class="overlay">
+    <h2>Welcome to</h2>
+    <h2 id='brand'><?php bloginfo('name') ?></h2>
+  </div>
+
 </section>
 <?php include 'footer.php' ?>
